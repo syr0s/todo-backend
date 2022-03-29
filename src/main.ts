6d437fb27ea50507ba0afce075732b5f70fddeb2
syr0s <daniel.noetzel@gmail.com>
2022-03-29 11:20:32 +0200

    feat: :sparkles: base setup of the node.js / typescript project

---
+++ src/main.ts
@@ -0,0 +1,11 @@
+/**
+ * RESTful API of the `todo application`.
+ *
+ * This is the main entrypoint of the RESTful API. It will create
+ * a new `express.js` web server.
+ *
+ * @version 1.0.0
+ * @since 1.0.0
+ * @author Daniel Noetzel <daniel.noetzel@gmail.com>
+ *
+ */
